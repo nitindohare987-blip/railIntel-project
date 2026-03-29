867fca7c0a166f64596cc0c85e52777334848a94
Nitin Dohare <nitindohare987@gmail.com>
2026-03-29 16:21:55 +0530

    Updated RailIntel Project

---
+++ script.js
@@ -0,0 +1,32 @@
+function bookTicket(){
+    window.location.href ="booking.html";
+}
+
+
+function runningStatus(){
+    window.location.href = "running.html"
+}
+
+function rating(){
+    window.location.href ="rating.html"
+}
+
+function searchTrain(){
+    window.location.href="search.html";
+}
+
+function Reliability(){
+    window.location.href="reliability.html";
+}
+
+function alternative(){
+    window.location.href="alternative.html"
+}
+
+function food(){
+    window.location.href="food.html";
+}
+
+function Delay(){
+    window.location.href="delaymeasure.html";
+}
